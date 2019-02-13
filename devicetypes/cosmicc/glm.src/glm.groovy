0500007277e59ed01ea53ba822dcaccd2af272a9
Ian Perry <ianperry99@gmail.com>
2019-02-13 17:41:55 -0500

    update

--- devicetypes/cosmicc/glm.src/glm.groovy
+++ devicetypes/cosmicc/glm.src/glm.groovy
@@ -13,6 +13,18 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
+import groovy.json.JsonSlurper
+import com.google.common.base.Splitter;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+preferences {
+        input("ip", "string", title:"IP Address", description: "192.168.1.150", defaultValue: "192.168.1.150" ,required: true, displayDuringSetup: true)
+        input("port", "string", title:"Port", description: "80", defaultValue: "80" , required: true, displayDuringSetup: true)
+        input("token", "string", title:"API Token", description: "apitoken", defaultValue: "0000000" , required: true, displayDuringSetup: true)
+}
+
 metadata {
 	definition (name: "GLM", namespace: "cosmicc", author: "Ian Perry") {
 		capability "Activity Lighting Mode"
@@ -50,14 +62,103 @@ metadata {
 		// TODO: define status and reply messages here
 	}
 
-	tiles {
-		// TODO: define your main and details tiles here
-	}
+tiles {
+	valueTile("temperature", "device.temperature", width: 1, height: 1) {
+            state "temperature", label:'${currentValue}° ', unit: "F",
+            backgroundColors:[
+                [value: 25, color: "#153591"],
+                [value: 35, color: "#1e9cbb"],
+                [value: 47, color: "#90d2a7"],
+                [value: 59, color: "#44b621"],
+                [value: 67, color: "#f1d801"],
+                [value: 76, color: "#d04e00"],
+                [value: 77, color: "#bc2323"]
+            ]
+        }
+        standardTile("button", "device.switch", width: 1, height: 1, canChangeIcon: true) {
+			state "off", label: 'Off', icon: "st.Electronics.electronics18", backgroundColor: "#ffffff", nextState: "on"
+			state "on", label: 'On', icon: "st.Electronics.electronics18", backgroundColor: "#79b821", nextState: "off"
+		}
+        valueTile("cpuPercentage", "device.cpuPercentage", inactiveLabel: false) {
+        	state "default", label:'${currentValue}% CPU', unit:"Percentage",
+            backgroundColors:[
+                [value: 31, color: "#153591"],
+                [value: 44, color: "#1e9cbb"],
+                [value: 59, color: "#90d2a7"],
+                [value: 74, color: "#44b621"],
+                [value: 84, color: "#f1d801"],
+                [value: 95, color: "#d04e00"],
+                [value: 96, color: "#bc2323"]
+            ]
+        }
+        valueTile("memory", "device.memory", width: 1, height: 1) {
+        	state "default", label:'${currentValue} MB', unit:"MB",
+            backgroundColors:[
+                [value: 353, color: "#153591"],
+                [value: 287, color: "#1e9cbb"],
+                [value: 210, color: "#90d2a7"],
+                [value: 133, color: "#44b621"],
+                [value: 82, color: "#f1d801"],
+                [value: 26, color: "#d04e00"],
+                [value: 20, color: "#bc2323"]
+            ]
+        }
+        valueTile("diskUsage", "device.diskUsage", width: 1, height: 1) {
+        	state "default", label:'${currentValue}% Disk', unit:"Percent",
+            backgroundColors:[
+                [value: 31, color: "#153591"],
+                [value: 44, color: "#1e9cbb"],
+                [value: 59, color: "#90d2a7"],
+                [value: 74, color: "#44b621"],
+                [value: 84, color: "#f1d801"],
+                [value: 95, color: "#d04e00"],
+                [value: 96, color: "#bc2323"]
+            ]
+        }
+        standardTile("contact", "device.contact", width: 1, height: 1) {
+			state("closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#79b821", action: "open")
+			state("open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ffa81e", action: "close")
+		}
+        standardTile("restart", "device.restart", inactiveLabel: false, decoration: "flat") {
+        	state "default", action:"restart", label: "Restart", displayName: "Restart"
+        }
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat") {
+        	state "default", action:"refresh.refresh", icon: "st.secondary.refresh"
+        }
+        main "button"
+        details(["button", "temperature", "cpuPercentage", "memory" , "diskUsage", "contact", "restart", "refresh"])
+    }
 }
 
 // parse events into attributes
 def parse(String description) {
 	log.debug "Parsing '${description}'"
+    def map = [:]
+    def descMap = parseDescriptionAsMap(description)
+    log.debug "descMap: ${descMap}"
+    
+    def body = new String(descMap["body"].decodeBase64())
+    log.debug "body: ${body}"
+    
+    def slurper = new JsonSlurper()
+    def result = slurper.parseText(body)
+    
+    log.debug "result: ${result}"
+
+	if (result){
+    	log.debug "Device is ALIVE"
+   		sendEvent(name: "switch", value: "on")
+    }
+    
+    if (result.containsKey("tempurature")) {
+        log.debug "tempurature: ${result.tempurature} F"
+    	sendEvent(name: "temperature", value: result.tempurature)
+    }
+    
+    if (result.containsKey("humidity")) {
+    	log.debug "humidity: ${result.humidity}"
+        sendEvent(name: "humidity", value: result.humidity)
+    }
 	// TODO: handle 'lightingMode' attribute
 	// TODO: handle 'activity' attribute
 	// TODO: handle 'presence' attribute
@@ -151,7 +252,7 @@ def poll() {
 
 def refresh() {
 	log.debug "Executing 'refresh'"
-	// TODO: handle 'refresh' command
+	postAction("GET", "/api/poll")
 }
 
 def setLevel() {
@@ -162,4 +263,83 @@ def setLevel() {
 def reset() {
 	log.debug "Executing 'reset'"
 	// TODO: handle 'reset' command
+}
+
+def postAction(meth, uri) {
+	log.debug "${meth} - ${uri}"
+    def result = new physicalgraph.device.HubAction(
+        method: meth,
+        path: uri,
+        headers: [
+            HOST: getHostAddress()
+        ]
+    )
+    log.debug "result: ${result}"
+    return result
+}
+
+def parseDescriptionAsMap(description) {
+	description.split(",").inject([:]) { map, param ->
+		def nameAndValue = param.split(":")
+		map += [(nameAndValue[0].trim()):nameAndValue[1].trim()]
+	}
+}
+
+
+def toAscii(s){
+        StringBuilder sb = new StringBuilder();
+        String ascString = null;
+        long asciiInt;
+                for (int i = 0; i < s.length(); i++){
+                    sb.append((int)s.charAt(i));
+                    sb.append("|");
+                    char c = s.charAt(i);
+                }
+                ascString = sb.toString();
+                asciiInt = Long.parseLong(ascString);
+                return asciiInt;
+    }
+
+private encodeCredentials(username, password){
+	log.debug "Encoding credentials"
+	def userpassascii = "${username}:${password}"
+    def userpass = "Basic " + userpassascii.encodeAsBase64().toString()
+    //log.debug "ASCII credentials are ${userpassascii}"
+    //log.debug "Credentials are ${userpass}"
+    return userpass
+}
+
+private getHeader(userpass){
+	log.debug "Getting headers"
+    def headers = [:]
+    headers.put("HOST", getHostAddress())
+    headers.put("Authorization", userpass)
+    //log.debug "Headers are ${headers}"
+    return headers
+}
+
+private delayAction(long time) {
+	new physicalgraph.device.HubAction("delay $time")
+}
+
+private setDeviceNetworkId(ip,port){
+  	def iphex = convertIPtoHex(ip)
+  	def porthex = convertPortToHex(port)
+  	device.deviceNetworkId = "$iphex:$porthex"
+  	log.debug "Device Network Id set to ${iphex}:${porthex}"
+}
+
+private getHostAddress() {
+	return "${ip}:${port}"
+}
+
+private String convertIPtoHex(ipAddress) { 
+    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
+    return hex
+
+}
+
+private String convertPortToHex(port) {
+	String hexport = port.toString().format( '%04x', port.toInteger() )
+    return hexport
 }
